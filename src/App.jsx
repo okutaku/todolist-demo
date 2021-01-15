32c284ef57ff2bd7fd8745c592ac2b22400242ee
okutaku <pntgn.taku@gmail.com>
2021-01-15 12:56:00 +0900

    cssの追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,41 @@
 import React from 'react';
+import "./index.css";
 
 export const App = () => {
   return (
-    <>
-    <div>
-      <input placeholder="やるべきこと"/>
-      <button>追加</button>
+    <div className="app">
+      <div className="input-area">
+        <input placeholder="やるべきこと"/>
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          <div className="list">
+            <li>1項目</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+          <div className="list">
+            <li>2項目</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
+        <ul>
+          <div className="list">
+            <li>1項目</li>
+            <button>戻す</button>
+          </div>
+          <div className="list">
+            <li>2項目</li>
+            <button>戻す</button>
+          </div>
+        </ul>
+      </div>
     </div>
-    <div>
-      <p>未完了のTODO</p>
-      <ul>
-        <div>
-          <li>1項目</li>
-          <button>完了</button>
-          <button>削除</button>
-        </div>
-        <div>
-          <li>2項目</li>
-          <button>完了</button>
-          <button>削除</button>
-        </div>
-      </ul>
-    </div>
-
-    <div>
-      <p>完了のTODO</p>
-      <ul>
-        <div>
-          <li>1項目</li>
-          <button>戻す</button>
-        </div>
-        <div>
-          <li>2項目</li>
-          <button>戻す</button>
-        </div>
-      </ul>
-    </div>
-    </>
   )
 }
